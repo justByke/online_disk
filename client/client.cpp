080e7b97e6ab9f168545625cc25410aa13aadb0b
byke <justbyke@163.com>
2017-05-23 22:49:49 +0800

    recv mistake on server

--- client/client.cpp
+++ client/client.cpp
@@ -8,9 +8,19 @@
 #include <netdb.h>//gethostbyname
 #include <unistd.h>//close
 #include <time.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 #define SERVER_PORT 8888
 #define BUFFER_SIZE 1024
+
+struct Command
+{
+	char cmd[20];
+	char file[64];
+};
 
 struct Addr
 {
@@ -18,44 +28,37 @@ struct Addr
 	int port;
 };
 
-int file_push(struct Addr addr,char *filename)
+struct User 
 {
-	struct sockaddr_in servAddr;
-	struct hostent* host;
-	int sockfd;
+	int uid;
+	char user_name[20];
+	char passwd[64];
+};
+
+int file_push(int sockfd,char *filename)
+{
+	char s[32];
 	FILE *fp;
-
-	host = gethostbyname(addr.host);
-	servAddr.sin_family = AF_INET;
-	servAddr.sin_port = htons(SERVER_PORT);
-	//servAddr.sin_addr = *((struct in_addr *)host->h_addr);
-	servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	
-	if(host == NULL) {
-		perror("获取IP地址失败\n");
-		exit(-1);
-	}
-
-	if((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
-		perror("socket 失败\n");
-		exit(-1);
-	}
-
-	if(connect(sockfd,(struct sockaddr *)&servAddr,sizeof(servAddr)) == -1) {
-		perror("connect failed\n");
-		exit(-1);
-	}
 
 	if((fp =fopen(filename,"rb")) == NULL) {
 		perror("file open failed\n");
 		exit(-1);
 	}
 
+	fseek(fp,0,SEEK_END);
+	int file_len = ftell(fp);
+	printf("%d\n",file_len);
+	sprintf(s,"%d",file_len);
+	send(sockfd,s,strlen(s),0);
+	rewind(fp);
+
 	char buf[BUFFER_SIZE];
 	bzero(buf,BUFFER_SIZE);
 	int len = 0;
 	while((len = fread(buf,1,BUFFER_SIZE,fp)) > 0)
 	{
+		write(1, buf, BUFFER_SIZE);
+
 		if(send(sockfd,buf,len,0) < 0)
 		{
 			perror("send file failed\n");
@@ -67,16 +70,73 @@ int file_push(struct Addr addr,char *filename)
 	}
 
 	fclose(fp);
-	close(sockfd);
 
+	//close(sockfd);
 	return 0;
+}
+
+int check_login(int sockfd,struct User *user)
+{
+
+	if(send(sockfd,(char *)user,sizeof(struct User),0) < 0)
+	{
+		perror("发送数据失败\n");
+		exit(-1);
+	}
+
+	bzero(user,sizeof(struct User));
+
+	if(recv(sockfd,(char *)user,sizeof(struct User),0) < 0)
+	{
+		perror("接收数据失败\n");
+		exit(-1);
+	}
+
+	printf("获取后用户名：%s,密码：%s,uid：%d\n",user->user_name,user->passwd,user->uid);
+
+
+	return user->uid;
+}
+
+int socket_connect(struct Addr addr)
+{
+	struct sockaddr_in servaddr;
+	struct hostent *host;
+	int sockfd;
+
+	host = gethostbyname(addr.host);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(addr.port);
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	if(host == NULL)
+	{
+		perror("IP 获取失败\n");
+		exit(-1);
+	}
+
+	if((sockfd = socket(AF_INET,SOCK_STREAM,0)) == -1)
+	{
+		perror("socket 创建失败\n");
+		exit(-1);
+	}
+
+	if(connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr)) == -1)
+	{
+		perror("connect 失败\n");
+		exit(-1);
+	}
+
+	return sockfd;
 }
 
 int main(int argc,char **argv)
 {
-	if(argc != 3)
+	struct User user;
+
+	if(argc != 5)
 	{
-		printf("usage: %s  addr  port\n",argv[0]);
+		printf("usage: %s  addr  port  username passwd\n",argv[0]);
 		exit(-1);
 	}
 
@@ -85,15 +145,48 @@ int main(int argc,char **argv)
 
 	strcpy(addr.host,argv[1]);
 	addr.port = atoi(argv[2]);
+	strcpy(user.user_name,argv[3]);
+	strcpy(user.passwd,argv[4]);
 
+	int sockfd = socket_connect(addr);
+
+	int uid = check_login(sockfd,&user);
+	if(uid <= 0)
+	{
+		perror("验证失败\n");
+		exit(-1);
+	}
+	
+	char cmd[BUFFER_SIZE];
+	struct Command command;
+	while(fgets(cmd,BUFFER_SIZE,stdin) != NULL)
+	{
+		cmd[strlen(cmd)-1] = 0;
+		if(strcmp(cmd,"quit") == 0)
+			break;
+		if(sscanf(cmd,"%s%s",&command.cmd,&command.file) != 2)
+		{
+			printf("input error\n");
+			continue;
+		}
+	//	bzero(cmd,sizeof(BUFFER_SIZE));
+	//	memcpy(cmd,&command,sizeof(struct Command));
+		send(sockfd,cmd,strlen(cmd),0);
+		printf("%s\t%s\n",command.cmd,command.file);
+		if(strcmp(command.cmd,"push") == 0)
+			file_push(sockfd,command.file);
+		bzero(cmd,sizeof(BUFFER_SIZE));
+	}	
+
+	/*
 	while(1)
 	{
 		printf("\n输入文件名:");
 		fgets(filename,BUFFER_SIZE,stdin);
 		filename[strlen(filename)-1] = 0;
-		file_push(addr,filename);
+		file_push(sockfd,filename);
 	}
-
+	*/
 	return 0;
 }
 
--- server/server.cpp
+++ server/server.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <mysql/mysql.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -9,13 +10,65 @@
 #include <arpa/inet.h>
 
 #define SERVER_PORT 8888
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 8888
+
+#define FILE_PUSH 1;
+#define FILE_PULL 2;
+#define FILE_LIST 3;
+#define FILE_SENDTO 4;
+#define FILE_DELETE 5;
+
+struct User
+{
+	int uid;
+	char user_name[20];
+	char passwd[64];
+};
+
+struct Control
+{
+	int uid;
+	int control;
+};
 
 void print_time(char *ch);
+int mysql_check_login(struct User user);
+void push_file(int connfd,struct sockaddr_in clientaddr);
+int read_line(int fd, char *vptr, ssize_t maxlen)
+{
+	ssize_t n, rc;
+	char c, *ptr;
+
+	ptr = vptr;
+	for (n = 1; n < maxlen; n++)
+	{
+		if ((rc = read(fd, &c, 1)) == 1)
+		{
+			*ptr++ = c;
+			if (c == '\n')
+				break;
+		}
+		else if (rc == 0)
+		{
+			*ptr = 0;
+			return(n - 1);
+		} 
+		else
+		{
+			return(-1);
+		}
+	}
+	*ptr = 0;
+	return(n);
+}
+
 
 int main(int argc,char **argv)
 {
-	struct sockaddr_in servaddr;
+	struct User user;
+	struct Control control;
+	struct sockaddr_in servaddr,client_addr;
+	char ch[64];
 	bzero(&servaddr,sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERVER_PORT);
@@ -50,56 +103,55 @@ int main(int argc,char **argv)
 
 		else {
 			printf("client %s : %d connect successful\n",inet_ntoa(clientaddr.sin_addr),ntohs(clientaddr.sin_port));
+			print_time(ch);
+			printf("加入的时间是:%s\n",ch);
+
+
 			pid = fork();
 			if(pid < 0) {
 				perror("fork failed\n");
 			}
 			else if(pid == 0) {
-				char buf[BUFFER_SIZE];
-				int data_len;
-				FILE *fp = NULL;
-				bzero(buf,BUFFER_SIZE);
-				if((fp = fopen("data","wb"))==NULL)
+				if(recv(connfd,(char *)&user,sizeof(struct User),0) < 0)
 				{
-					perror("FILE open failed\n");
+					perror("shuju jie shou cuo wu\n");
 					exit(-1);
 				}
-				int size = 0;// block
-
-				while(data_len = recv(connfd,buf,BUFFER_SIZE,0)) {
-					if(data_len < 0) {
-						perror("data recv mistake\n");
-						exit(-1);
-					}
-					size++;
-					if(size == 1)
-					{
-						printf("正在接收来自%s : %d 的文件\n",inet_ntoa(clientaddr.sin_addr),ntohs(clientaddr.sin_port));
-					}					
-					else
-						printf(".");
-					//向文件中写入
-					int write_len = fwrite(buf,sizeof(char),data_len,fp);
-					if(write_len > data_len) {
-						printf("写入错误\n");
-						exit(-1);
-					}
-					bzero(buf,BUFFER_SIZE);
+				printf("客户端发送过来的用户名是:%s,密码是:%s\n",user.user_name,user.passwd);
+				if((user.uid = mysql_check_login(user)) <= 0)
+				{
+					printf("身份验证失败\n");
+					exit(-1);
 				}
+				printf("身份验证成功\n");
+				send(connfd,(char *)&user,sizeof(struct User),0);
+			//在验证成功之后将这个客户的目录发送过去显示.	
 				
-				if(size > 0)
-					printf("%s : %d 的文件传送成功\n",inet_ntoa(clientaddr.sin_addr),ntohs(clientaddr.sin_port));
-				else
-					printf("%s : %d 的文件传送失败\n",inet_ntoa(clientaddr.sin_addr),ntohs(clientaddr.sin_port));
-				fclose(fp);
+
+				char cmd[20];
+				char file[64];
+				char line[BUFFER_SIZE];
+				recv(connfd,line,BUFFER_SIZE,0);
+				printf("%s\n",line);
+				if(sscanf(line,"%s%s",cmd,file) == 2)
+				{
+					printf("%s%s\n",cmd,file);
+					if(strcmp(cmd,"push") == 0)
+						push_file(connfd,clientaddr);
+				}
+				/*
+				while(1)	
+				{
+					push_file(connfd,clientaddr);
+				}
 				exit(0);
+				*/
 			}
 			else {
-				;
+				close(connfd);
 			}
 
 		}
-		close(connfd);
 	}
 
 	return 0;
@@ -116,9 +168,128 @@ void print_time(char *ch)
 	return ;
 }
 
+int mysql_check_login(struct User user)
+{
+	MYSQL conn;
+	MYSQL_RES *res_ptr;
+	MYSQL_ROW result_row;
+	int res;
+	int row;
+	int column;
+	int uid;
+	char sql[256] = {0};
+	strcpy(sql,"select uid from user where username=\"");
+	strcat(sql,user.user_name);
+	strcat(sql,"\" and passwd=\"");
+	strcat(sql,user.passwd);
+	strcat(sql,"\";");
+	printf("查询的sql : %s\n",sql);
+
+	uid = -1;
+	mysql_init(&conn);
+	if(mysql_real_connect(&conn,"127.0.0.1","root","sujian","online_disk",0,NULL,CLIENT_FOUND_ROWS))
+	{
+		res = mysql_query(&conn,sql);
+		if(res)
+		{
+			perror("SELECT SQL ERROR\n");
+			exit(-1);
+		}
+		else
+		{
+			res_ptr = mysql_store_result(&conn);
+			if(res_ptr)
+			{
+				column = mysql_num_fields(res_ptr);//获取列数
+				row = mysql_num_rows(res_ptr)+1;//获取行数，+1表示还有第一行字段名
+				if(row <= 1)
+					;
+				else
+				{
+					result_row = mysql_fetch_row(res_ptr);
+					printf("获取到UID是: %s\n",result_row[0]);
+					uid = atoi(result_row[0]);
+				}
+			}
+			else
+			{
+				printf("没有查询到匹配的数据\n");
+			}
+		}
+	}
+	else
+	{
+		perror("connect error\n");
+		exit(-1);
+	}
+
+	mysql_close(&conn);
+	return uid;
+}
 
 
+void push_file(int connfd,struct sockaddr_in clientaddr)
+{
+	char s[32];
+	bzero(s,32);
+	int file_len = 0;
+	int fd = read_line(connfd,s,32);
+	if(fd == 0)
+	{
+		printf("客户端断开连接\n");
+		exit(0);
+	}
+	file_len = atoi(s);
+	//printf("%d\n",file_len);
+	char buf[BUFFER_SIZE];
+	int data_len;
+	FILE *fp = NULL;
+	if((fp = fopen("data","wb"))==NULL)
+	{
+		perror("FILE open failed\n");
+		exit(-1);
+	}
+	int size = 0;// block
 
+	int len = 0;
+	bzero(buf,BUFFER_SIZE);
+	while(data_len = recv(connfd,buf,BUFFER_SIZE,0)) {
+	
+			write(1, buf, data_len);
+			if(data_len < 0) {
+				perror("data recv mistake\n");
+				exit(-1);
+			}
+			size++;
+			if(size == 1)
+			{
+				printf("正在接收来自%s : %d 的文件\n",inet_ntoa(clientaddr.sin_addr),ntohs(clientaddr.sin_port));
+			}					
+			else
+				printf(".");
+			//向文件中写入
+			int write_len = fwrite(buf,sizeof(char),data_len,fp);
+		
+			if(write_len > data_len) {
+				printf("写入错误\n");
+				exit(-1);
+			}
+			bzero(buf,BUFFER_SIZE);
+			len = len + data_len;
+			printf("%d\n",len);
+			if(len >= file_len)
+				break;
+	}
+
+	
+	if(size > 0)
+		printf("%s : %d 的文件传送成功\n",inet_ntoa(clientaddr.sin_addr),ntohs(clientaddr.sin_port));
+	else
+		printf("%s : %d 的文件传送失败\n",inet_ntoa(clientaddr.sin_addr),ntohs(clientaddr.sin_port));
+	fclose(fp);
+//	exit(0);
+
+}
 
 
 
